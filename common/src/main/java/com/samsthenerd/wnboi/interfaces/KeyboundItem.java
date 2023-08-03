63bd752123147a1f724da51a8787fd0ea5d2851d
SamsTheNerd <sam@samsthenerd.com>
2023-08-03 12:52:53 -0400

    try to fix server-y stuff?

--- common/src/main/java/com/samsthenerd/wnboi/interfaces/KeyboundItem.java
+++ common/src/main/java/com/samsthenerd/wnboi/interfaces/KeyboundItem.java
@@ -12,15 +12,18 @@ import net.minecraft.client.option.KeyBinding;
 public interface KeyboundItem{
     // override this if you want to use a different keybinding
     // see KeybindUtils.DEFAULT_KEYBINDING for an example of how to make a new one
+    @Environment(EnvType.CLIENT)
     public default KeyBinding getKeyBinding(){
         return KeybindUtils.DEFAULT_KEYBINDING;
     }
 
     // needs to return the relevant instance of the screen. so make a new one or grab the current one if it's already open
     // can check with (MinecraftClient.getInstance().currentScreen instanceof YourContextWheelScreen) 
+    @Environment(EnvType.CLIENT)
     public AbstractContextWheelScreen getScreen();
 
     // checks if the screen is open. the default impl may not work depending on how you implement getScreen()
+    @Environment(EnvType.CLIENT)
     public default boolean screenIsOpen(){
         return MinecraftClient.getInstance().currentScreen == getScreen();
     }
